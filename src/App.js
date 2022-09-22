c22fdbc0e12f14ff12b9025d4bd53c950f46b969
prosagar-ggs <104219529+prosagar-ggs@users.noreply.github.com>
2022-09-22 16:36:35 +0530

    OTT APP

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import Movielist from './components/MovieList';
+import MovieListHeading from './components/MovieListHeading';
+import SearchBox from './components/SearchBox';
+import AddFavourite from './components/AddFavourites';
+import RemoveFavourites from './components/RemoveFavourites';
 
 function App() {
+  const [movies, setMovies] = useState([]);
+  const [searchValue,setSearchValue] = useState("");
+  const [favourites,setFavourites] = useState([]);
+
+  const getMovieRequest = async (searchValue) => {
+    const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=809f60d4`;
+
+    const response = await fetch(url);
+    const responseJson = await response.json();
+
+    if (responseJson.Search){
+      setMovies(responseJson.Search);
+    }
+  };
+
+  useEffect(() =>{
+    getMovieRequest(searchValue);
+  }, [searchValue]);
+
+  const saveToLocalStorage = (items) =>{
+    localStorage.setItem("react-ott-app-favourites", JSON.stringify(items));
+  };
+
+  const AddFavouriteMovie = (movie) => {
+    const newFavouriteList = [...favourites,movie];
+    setFavourites(newFavouriteList);
+    saveToLocalStorage(newFavouriteList);
+  };
+
+
+  const removeFavouriteMovie = (movie) => {
+    const newFavouriteList = favourites.filter(
+      (favourite) => favourite.imdbID !== movie.imdbID
+    );
+    setFavourites(newFavouriteList);
+    saveToLocalStorage(newFavouriteList);
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container-fluid ott-app">
+      <div className="row d-flex align-items-center mt-4 mb-4">
+        <MovieListHeading heading="Movies" />
+        <SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
+      
+      </div>
+      <div className="row">
+        <Movielist
+          movies={movies}
+          handleFavoritesClick={AddFavouriteMovie}
+          favouriteComponent = {AddFavourite}
+        />
+      </div>
+      <div className="row d-flex align-items-center mt-4 mb-4">
+        <MovieListHeading heading="Favourites" />
+      </div>
+      <div className="row">
+        <Movielist
+          movies={favourites}
+          handleFavoritesClick={removeFavouriteMovie}
+          favouriteComponent = {RemoveFavourites}
+        />
+      
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/MovieList.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const MovieList =(props)=>{
+    const FavouriteComponent = props.favouriteComponent;
+
+
+    return(
+        <>
+            {props.movies.map((movie,index) =>(
+                <div className="image-container d-flex justify-content-start m-3">
+                
+                    <img src={movie.Poster} alt="movie" />
+                    <div
+                        onClick = {()=> props.handleFavoritesClick(movie)}
+                        className="overlay d-flex align-items-center justify-content-center"
+                    >
+                        <FavouriteComponent/>
+                    </div>
+                </div>
+
+
+            ))}
+        
+        </>
+
+    );
+}
+export default MovieList;
